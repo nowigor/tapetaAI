05f0b29dcf6fdb2d52292ed4ab88f6601251cc66
Igor_now <igornowak10@gmail.com>
2024-11-22 21:49:53 +0100

    first commit

---
+++ app/desktop/main.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require('electron/main')
+const path = require('node:path')
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      devTools: true 
+    },
+    icon: path.join(__dirname, 'icona.ico') 
+  })
+
+  win.loadFile(path.join(__dirname, 'build', 'index.html'))
+  win.webContents.openDevTools() 
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
