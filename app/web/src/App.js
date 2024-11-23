a2fa823b3a4b0c2d6105dcb72591f1d63c063b1a
Igor_now <igornowak10@gmail.com>
2024-11-23 09:57:18 +0100

    Bootstrap i jakiej pierdy

--- app/web/src/App.js
+++ app/web/src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 import GUI from './Compontens/GUI';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 function App() {
   return (
